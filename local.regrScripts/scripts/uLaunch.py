da6aeb5e1d6529d5d86d5919d0edf2446d19aa80
Manxinator <manxinator@gmail.com>
2015-10-18 15:58:29 -0700

    Implemented spawning of pre-run and post-run

--- local.regrScripts/scripts/uLaunch.py
+++ local.regrScripts/scripts/uLaunch.py
@@ -33,45 +33,63 @@ from os.path import basename
 
 g_child_pid = None
 g_progName  = None
+g_verbose   = None
 
 #-------------------------------------------------------------------------------
 class launchDelegate:
   def __init__(self):
     self.prog = basename(sys.argv[0])
-    global g_progName
-    g_progName = self.prog
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose',   action='count',                                help='Increase verbosity')
+    parser.add_argument('-v', '--verbose',   action='count',                                  help='Increase verbosity')
     parser.add_argument('-l', '--logFile',   nargs=1,   dest='logFile',   default='run_log',  help='Log File')
     parser.add_argument('-r', '--runSeed',   nargs=1,   dest='runSeed',                       help='Random seed')
     parser.add_argument('-P', '--preRun',    nargs=1,   dest='preRun',                        help='Comma-separated list of pre-run scripts')
     parser.add_argument('-p', '--postRun',   nargs=1,   dest='postRun',                       help='Comma-separated list of post-run scripts')
     parser.add_argument('-x', '--extraArgs', nargs='+', dest='extraArgs',                     help='Arguments for the executable')
-    parser.add_argument('bin',                                                              help='Executable program to launch')
+    parser.add_argument('bin',                                                                help='Executable program to launch')
 
     self.args = parser.parse_args()
     print "\n-----"
     print "- ", self.args
     print "-"
 
-  def launchAndWait(self,execFile,logHand,exArgs):
-    cmd = [self.args.bin] + exArgs
+    #= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    global g_progName
+    global g_verbose
+    g_progName = self.prog
+    g_verbose  = 0 if self.args.verbose is None else self.args.verbose
+    g_verbose  = 10 # Temporary override
 
-    print "[%s] %s; launching:" %(self.prog, datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+
+  def launchAndWait(self,execFile,logHand,exArgs=[]):
+    cmd = [execFile] + exArgs
+
+    print "[%s] launching: %s, [%s]" %(self.prog, cmd[0], datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
     print "[%s] %s" %(self.prog,cmd)
 
     # Run
-    global g_child_pid
+    logHand.write('[%s] * * * * * * * * * * launching %s\n' %(self.prog,cmd[0]) )
+    logHand.write('[%s] %s\n' %(self.prog,cmd) )
+    logHand.flush()
     childProc = subprocess.Popen(cmd, stdout=logHand, stderr=logHand, shell=False, preexec_fn=os.setsid)
+
+    global g_child_pid
     g_child_pid = childProc.pid
 
     # Wait via polling, since I couldn't figure out how to kill
     childProc.wait()
 
     g_child_pid = None
-    print "[%s] %s; Done!" %(self.prog, datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
-    print "--------------------------------------------------"
+    print "[%s] %s done" %(self.prog, cmd[0])
+
+
+  def launchList(self,cmdStr,logFileHand):
+    cmdList = cmdStr.split(",")
+    for oneScript in cmdList:
+      print "  + %s" % oneScript
+      self.launchAndWait(oneScript,logFileHand)
+
 
   def run(self):
     try:
@@ -80,14 +98,31 @@ class launchDelegate:
       sys.stderr.write("Failed to open %s for writing" %self.args.logFile[0])
       sys.exit(-1)
 
-    # TODO:
+    #---------------------------------------------------------------------------
     # 1- run pre-run scripts in order
-    # 2- run program -- log file should be configurable
+    # 2- run program
     # 3- run post-run scripts in order
+    #---------------------------------------------------------------------------
+
+    # 1- run pre-run scripts
+    #
+    if self.args.preRun is not None:
+      print "------------------------- preRun: %s" % self.args.preRun
+      self.launchList(self.args.preRun[0],logFH)
 
     # 2- run program
     #
+    print "------------------------- run program"
     self.launchAndWait(self.args.bin,logFH,self.args.extraArgs)
+
+    # 3- run post-run scripts
+    #
+    if self.args.postRun is not None:
+      print "------------------------- postRun: %s" % self.args.preRun
+      self.launchList(self.args.postRun[0],logFH)
+
+    print "[%s] %s; Done!" %(self.prog, datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+    print "--------------------------------------------------"
 
 
   #-----------------------------------------------------------------------------
@@ -97,11 +132,14 @@ class launchDelegate:
     #
     global g_child_pid
     global g_progName
+    global g_verbose
 
     if g_child_pid is None:
-      sys.stderr.write("\n[%s] launcherCleanup() g_child_pid is None" % g_progName)
+      if g_verbose > 0:
+        sys.stderr.write("\n[%s] launcherCleanup() g_child_pid is None" % g_progName)
     else:
-      sys.stderr.write("\n[%s] launcherCleanup() killing g_child_pid=%d" % (g_progName,g_child_pid))
+      if g_verbose > 0:
+        sys.stderr.write("\n[%s] launcherCleanup() killing g_child_pid=%d" % (g_progName,g_child_pid))
       os.kill(g_child_pid, signal.SIGTERM)
 
   import atexit
